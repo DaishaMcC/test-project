c7f59afcea059105ec4456955c53010b47400667
Daisha McCutcheon <daisha05311994@gmail.com>
2025-12-15 23:11:21 -0500

    Initial Commit

---
+++ test-project/test_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_projectApp.swift
+//  test-project
+//
+//  Created by Daisha McCutcheon on 12/15/25.
+//
+
+import SwiftUI
+
+@main
+struct test_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
